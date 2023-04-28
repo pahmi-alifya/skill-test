287a6c57d2d33d66daad76985fa8ca657c77cddc
Pahmi Alifya Bahri <pahmi.bahri@mncgroup.com>
2023-04-28 21:22:57 +0700

    chore(env): for handle multiple env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
+const webpack = require("webpack");
 
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack: (config) => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+
+    config.plugins.push(new webpack.DefinePlugin(env));
+    return config;
   },
 };
 
