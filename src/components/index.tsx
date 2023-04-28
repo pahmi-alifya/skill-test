cfed49071b4d9434518eb306a8bb435c6a43b159
Pahmi Alifya Bahri <pahmi.bahri@mncgroup.com>
2023-04-28 21:15:27 +0700

    feat(comp): export all component

---
+++ src/components/index.tsx
@@ -0,0 +1,6 @@
+export { default as Table } from "./Table";
+export { default as PopoverDelete } from "./popover/Delete";
+export { default as Modal } from "./Modal";
+export { default as Dropdown } from "./Dropdown";
+export { default as Filter } from "./Filter";
+export * from "./Icons";
